2d7ac51af063caacbf6ad51d3bd10697661362d7
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-08 19:40:08 +0530

    Code_Update

--- objects.js
+++ objects.js
@@ -1,8 +1,12 @@
 
 
+const mySym = Symbol("Key1")
+
+
 const JsUser = {
     name: "Abhigyan",
     "full name": "Abhiyan Kumar Singh",
+    [mySym]: "mykey1",
     age:20,
     location: "ranchi",
     email: "Abhi@gmal.com",
@@ -17,4 +21,6 @@ console.log(JsUser["email"]); // It is also a way to write
 // We can not do console.log(JsUser.name) for "full name"
 
 console.log(JsUser["full name"]);
+
+console.log(JsUser[mySym])
 
