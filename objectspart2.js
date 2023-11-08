370eeeebc18d7c162d9497dc67d9f2530fc0d3b3
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-08 21:05:56 +0530

    Code_update

---
+++ objectspart2.js
@@ -0,0 +1,24 @@
+//const tinderUser = new Object() //Songleton pbject
+
+const tinderUser = {} //Non-Singlelton Object
+
+
+tinderUser.id = "123abc"
+
+tinderUser.name = "Sammy"
+
+tinderUser.isLogged = false
+
+//console.log(tinderUser);
+
+
+const regularUser = {
+    email: "SOme @gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "Abhi",
+            lastname: "Singh"
+        }
+    }
+}
+
