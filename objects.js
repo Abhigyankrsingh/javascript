871572b62e2072f7944fafa05d3415c75f1f838d
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-08 18:57:57 +0530

    Code Added

---
+++ objects.js
@@ -0,0 +1,11 @@
+
+
+const JsUser = {
+    name: "Abhigyan",
+    "full name": "Abhiyan Kumar Singh",
+    age:20,
+    location: "ranchi",
+    email: "Abhi@gmal.com",
+    isLogged: false,
+    lastLogininDays : ["Monday", "Saturday"]
+}
