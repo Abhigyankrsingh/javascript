67e770daba292fdf21a6597d99e7448e8ea27150
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-11-10 21:13:17 +0530

    Spread_Operator

--- objectspart2.js
+++ objectspart2.js
@@ -34,6 +34,11 @@ const obj4 = {5: "a", 6: "b"}
 //const obj3 = Object.assign(obj1, obj2)
 //const obj3 = Object.assign({}, obj1, obj2)
 
-const obj3 = Object.assign({}, obj1, obj2,obj3)
+// const obj3 = Object.assign({}, obj1, obj2,obj3)
+
+
+// best method
+
+const obj3 = {...obj1,...obj2}
 
  console.log(obj3);
