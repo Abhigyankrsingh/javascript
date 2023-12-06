7c3ad6d016b5a3761d1df678cf4d3553a6398ce6
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2023-12-06 20:45:57 +0530

    Code_Updated

--- obj.js
+++ obj.js
@@ -1,3 +1,17 @@
 //singleton
 
 //object literals
+
+const JsUser = {
+    name: "Abhi",
+    age:18,
+    location:"jaipur",
+    email: "abhi@gmail.com",
+    isLoggefIn: false,
+    last
+
+
+}
+
+
+//myArray = ["hi", "i"]
