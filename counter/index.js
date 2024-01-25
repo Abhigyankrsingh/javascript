ef7c5f62baca622dd90f8aad41f2437d4b09ee37
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-25 15:10:08 +0530

    Code_updated

--- counter/index.js
+++ counter/index.js
@@ -3,3 +3,9 @@ const decreaseBtn = document.getElementById("decreaseBtn");
 
 const resetBtn = document.getElementById("resetBtn");
 
+const increaseBtn = document.getElementById("increaseBtn");
+
+const countLabel = document.getElementById("countLabel");
+
+
+let count = 0;
