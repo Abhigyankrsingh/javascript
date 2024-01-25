28785eb8882cd69ab6a867e1d823be74fbf1ce48
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-25 19:12:30 +0530

    Code_Updated

--- checked/index.js
+++ checked/index.js
@@ -4,5 +4,18 @@
  const visaBtn = document.getElementById("visaBtn");
  const masterBtn = document.getElementById("masterBtn");
  const paypalBtn = document.getElementById("paypalBtn");
+ const mySubmit = document.getElementById("mySubmit");
+ const subResult = document.getElementById("subResult");
+ const paymentResult  = document.getElementById("paymentResult");
 
+
+ mySubmit.onclick = function() {
+
+    if(mycheckBox.checked){
+        subResult.textContent = `You are subscribed`;
+    }
+    else {
+        subResult.textContent = `You are NOT subscribed`;
+    }
+ }
  
