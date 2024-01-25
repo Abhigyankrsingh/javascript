1631b2d07499533526f259a24b964f1a9b7a75f9
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-01-25 19:50:01 +0530

    Code_Updated

--- checked/index.js
+++ checked/index.js
@@ -17,5 +17,10 @@
     else {
         subResult.textContent = `You are NOT subscribed`;
     }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with visa`;
+        
+    }
  }
  
