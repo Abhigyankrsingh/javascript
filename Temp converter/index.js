6f4a5e2766ab107910de78f9c712b1b39e1df457
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-04 12:39:32 +0530

    Code_Updated

--- Temp converter/index.js
+++ Temp converter/index.js
@@ -1,7 +1,13 @@
 
 
+const textBox = document.getElementById("textBox");
+
+const Tofarhen = document.getElementById("Tofarhen");
+
+const ToCelsius = document.getElementById("ToCelsius");
+
 
 function convert(){
 
-    
+
 }
