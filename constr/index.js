7b035259f94dabca41a35bebcbf44ba75be5f69a
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-06 15:24:13 +0530

    Code_Updated

---
+++ constr/index.js
@@ -0,0 +1,16 @@
+function Car(make, model, year, color){
+
+    this.make = make,
+
+    this.model = model,
+
+    this.year = year,
+
+    this.color = color
+
+}
+
+
+
+const car1 = new Car("ford", "m1", 2023, "blue");
+
