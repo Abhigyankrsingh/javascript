fc596be2d4be18b4fce7b494703e196bcd07201c
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-06 15:51:51 +0530

    Code_Updated

---
+++ class/index.js
@@ -0,0 +1,14 @@
+class Product {
+    constructor(name, year) {
+      this.name = name;
+      this.year = year;
+    }
+  
+    displayProduct() {
+      console.log(`This is the ${this.name} and it was manufactured in ${this.year}`);
+    }
+  }
+  
+  const product1 = new Product("Samsung", 1998);
+  product1.displayProduct();
+  
