469b6572eb1b85984e817cda269da2a27d1c8707
Abhigyankrsingh <singhabhigyankumar@gmail.com>
2024-02-07 23:03:51 +0530

    upate

--- class/index.js
+++ class/index.js
@@ -8,7 +8,7 @@ class Product {
       console.log(`This is the ${this.name} and it was manufactured in ${this.year}`);
     }
   }
-  
+  const product2 = new Product("Samsung", 1998);
   const product1 = new Product("Samsung", 1998);
   product1.displayProduct();
   
